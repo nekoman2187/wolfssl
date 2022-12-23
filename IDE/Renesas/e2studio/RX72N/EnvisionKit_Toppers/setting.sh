343547fea0a0de8af91f9717d96d574e1e23920e
watanabe <nekoman@mac.com>
2022-12-23 16:33:46 +0900

    Setting Script update

--- IDE/Renesas/e2studio/RX72N/EnvisionKit_Toppers/setting.sh
+++ IDE/Renesas/e2studio/RX72N/EnvisionKit_Toppers/setting.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# target path
+# param  path
 
 pathlist[0]="./asp/arch/rx_gcc/rx_common"
 pathlist[1]="./asp/arch/rx_gcc/rx72n"
@@ -10,12 +10,17 @@ pathlist[3]="./asp/sample"
 function convert () {
 echo hello_world
 echo $1
+sed -i -z 's/\r\n/\n/g' $1/*
+
 for file in `find $1 -type f`;
 do
     echo $file 
-   if [[ "$file" =~ rx63* ]] ; then
+#    iconv -f utf-8  -t euc-jp $file  > tmpfile
+    if [[ "$file" =~ rx63* ]] ; then
       nm=`echo  $file | sed -e "s/rx63*/rx72/g"` 
       mv $file $nm
+     # rm $file
+ 
     fi
 
 done
@@ -23,6 +28,8 @@ done
 rm -rf asp/arch/rx_gcc
 #copy target
 cp -fr ${1}/asp/arch/rx_gcc ./asp/arch/
+cp -fr ${1}/asp/arch/rx_gcc ./
+
 mv  ./asp/arch/rx_gcc/rx63n ./asp/arch/rx_gcc/rx72n
 rm -rf ./asp/arch/rx_gcc/rx64m
 cp -r ${1}/asp/target/sakura_gcc ./asp/arch/ 
@@ -41,6 +48,8 @@ rm -rf ${pathlist[2]}/target_device
 cp  -r ${1}/asp/sample ./asp
 cp  -f ${1}/asp/kernel/interrupt.c ./asp/kernel/
 cp  -f ${1}/asp/kernel/kernel_impl.h ./asp/kernel/
+sed -i -z 's/\r\n/\n/g' ./asp/kernel/interrupt.c
+sed -i -z 's/\r\n/\n/g' ./asp/kernel/kernel_impl.h
 
 #convert target
 {
